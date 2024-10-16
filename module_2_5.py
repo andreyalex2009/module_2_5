5999430c3edb9be826abea1b4e1773c5a081f17b
andreyalex2009 <andreyalex2009@yandex.ru>
2024-10-16 21:33:38 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,12 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+
+
+result = get_matrix(2, 2, 10)
+print(result)
